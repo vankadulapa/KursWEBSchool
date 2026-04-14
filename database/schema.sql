7e67518d073268939aa946b8dace8b8302fb62a9
vankadulapa <vankadulapa@gmail.com>
2026-04-14 14:53:03 +0300

    Initial commit

---
+++ database/schema.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict PI2FtVk16W4Or1LmCJuPOt9Kncnk9VpXAZ7pcWGozwVlZG2fOYQe1fQt3lwVgr7
+
+-- Dumped from database version 18.3
+-- Dumped by pg_dump version 18.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict PI2FtVk16W4Or1LmCJuPOt9Kncnk9VpXAZ7pcWGozwVlZG2fOYQe1fQt3lwVgr7
+
---
+++ database/seed.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict bB05x3wj9OrFIZFLz0eIfZ9adEnX0THs8ZN6EsbG6mt12LYFtLuIZbthIsSOL3S
+
+-- Dumped from database version 18.3
+-- Dumped by pg_dump version 18.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict bB05x3wj9OrFIZFLz0eIfZ9adEnX0THs8ZN6EsbG6mt12LYFtLuIZbthIsSOL3S
+
